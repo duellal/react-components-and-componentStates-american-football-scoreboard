c3232cee820994990a0c0bf04232b2e9272fc88b
duellal <alexandriaduell@gmail.com>
2022-07-04 17:58:17 -0400

    Took out the New Game button onClick and reverted the code back to make the other buttons work

--- src/App.js
+++ src/App.js
@@ -7,17 +7,19 @@ import "./App.css";
 function App() {
   //TODO: STEP 2 - Establish your applictaion's state with some useState hooks.  You'll need one for the home score and another for the away score.
 
-  {/* Trying to get the button "New Game" to reset board. Whenever another button other than "New Game" is pushed, the homeScore, awayScore and Quarter boxes all collapse and you cannot see the numbers. Unsure why this is happening*/ }
-  const initialState = {
-    homeScore: 0,
-    awayScore: 0,
-    quarter: 1
-  }
-  const [{ homeScore, awayScore, quarter }, setState] = useState(initialState)
+  {/* Trying to get the button "New Game" to reset board. Whenever another button other than "New Game" is pushed, the homeScore, awayScore and Quarter boxes all collapse and you cannot see the numbers. Unsure why this is happening. 
 
-  // const [homeScore, setHomeScore] = useState(0);
-  // const [awayScore, setAwayScore] = useState(0);
-  // const [quarter, setQuarter] = useState(1);
+  Commented out this code b/c I could not figure out how to make it work as well as make the other buttons work. */ }
+  // const initialState = {
+  //   homeScore: 0,
+  //   awayScore: 0,
+  //   quarter: 1
+  // }
+  // const [{ homeScore, awayScore, quarter }, setState] = useState(initialState)
+
+  const [homeScore, setHomeScore] = useState(0);
+  const [awayScore, setAwayScore] = useState(0);
+  const [quarter, setQuarter] = useState(1);
 
   return (
     <div className="container">
@@ -30,16 +32,19 @@ function App() {
         <div className="homeButtons">
 
           {/* TODO STEP 4 - Now we need to attach our state setter functions to click listeners. */}
-          <button className="homeButtons__touchdown" onClick={() => { setState(homeScore + 7) }}>Home Touchdown</button>
-          <button className="homeButtons__fieldGoal" onClick={() => { setState(homeScore + 3) }}>Home Field Goal</button>
+          <button className="homeButtons__touchdown" onClick={() => { setHomeScore(homeScore + 7) }}>Home Touchdown</button>
+          <button className="homeButtons__fieldGoal" onClick={() => { setHomeScore(homeScore + 3) }}>Home Field Goal</button>
         </div>
         <div className="awayButtons">
-          <button className="awayButtons__touchdown" onClick={() => { setState(awayScore + 7) }}>Away Touchdown</button>
-          <button className="awayButtons__fieldGoal" onClick={() => { setState(awayScore + 3) }}>Away Field Goal</button>
+          <button className="awayButtons__touchdown" onClick={() => { setAwayScore(awayScore + 7) }}>Away Touchdown</button>
+          <button className="awayButtons__fieldGoal" onClick={() => { setAwayScore(awayScore + 3) }}>Away Field Goal</button>
         </div>
         <div className='bottomRowButtons'>
-          <button className='nextQuarter' onClick={() => { if (quarter < 4) { setState(quarter + 1) } }}>Next Quarter</button>
-          <button className='newGame' onClick={() => { setState(initialState) }}> New Game </button>
+          <button className='nextQuarter' onClick={() => { if (quarter < 4) { setQuarter(quarter + 1) } }}>Next Quarter</button>
+          {/* Could not get the "New Game" button below to reset all the boxes
+          
+          Deleted the onClick function to get rid of the errors*/}
+          <button className='newGame'> New Game </button>
         </div>
       </section >
     </div >
